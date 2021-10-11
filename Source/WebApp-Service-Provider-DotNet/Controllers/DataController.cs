50771e3ee0fa5f4141051c3a67e4e294530b32f6
Amine A <15179425+AmineI@users.noreply.github.com>
2021-10-11 15:56:28 +0200

    Update switch case format

--- Source/WebApp-Service-Provider-DotNet/Controllers/DataController.cs
+++ Source/WebApp-Service-Provider-DotNet/Controllers/DataController.cs
@@ -230,15 +230,12 @@ namespace WebApp_Service_Provider_DotNet.Controllers
 
         private BaseResourceViewModel ConvertResource(string json, string scheme)
         {
-            switch (scheme)
+            return scheme switch
             {
-                case "DGFIP":
-                    return JsonSerializer.Deserialize<DgfipResourceViewModel>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-                case "Custom":
-                    return JsonSerializer.Deserialize<CustomResourceViewModel>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-                default:
-                    throw new NotImplementedException();
-            }
+                "DGFIP" => JsonSerializer.Deserialize<DgfipResourceViewModel>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }),
+                "Custom" => JsonSerializer.Deserialize<CustomResourceViewModel>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }),
+                _ => throw new NotImplementedException(),
+            };
         }
 
         private string GetResourceUrl(string providerName)
